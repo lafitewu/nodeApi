083c1652f795be8f98b4218de968090f52378bc3
吴丰池 <wufengchi@wufengchideMacBook-Pro.local>
2018-07-21 11:47:12 +0800

    add接口传参测试

--- db/usersql.js
+++ db/usersql.js
@@ -2,7 +2,7 @@ var UserSQL = {
 	insert:'INSERT INTO htt_tab_dynamic_list(id,title,resource,url,type,images,status,createDate) VALUES(?,?,?,?,?,?,?,NOW())', 
 	querypages: 'select id from htt_tab_dynamic_list',
 	queryAll:'SELECT id,type,title,info,resource,url,html,status,images FROM htt_tab_dynamic_list limit ?,?', 
-	queryAdmin: 'select * from htt_tab_dynamic_list',
+	queryAdmin: 'select * from htt_tab_dynamic_list limit ?,?',
 	queryImg:'SELECT images FROM htt_tab_dynamic_list', 
 	getUserById:'SELECT * FROM user WHERE id = ? ',
 	getUserByName:'SELECT * FROM user WHERE name = ? ',
--- routes/index.js
+++ routes/index.js
@@ -132,7 +132,7 @@ router.get('/yyt/cj/add', function(req, res, next) {
 	pool.getConnection(function(err, connection) { 
 		// 获取前台页面传过来的参数  
  		var param = req.query || req.params;
- 		datas = [10,'macpro？','阅条2','www2','2','http://img4.imgtn.bdimg.com/it/u=3552819207,3331878965&fm=27&gp=0.jpg','1'];
+ 		datas = param.datas;
  		connection.query(userSQL.insert, datas,function(err, result) {
 			if(result) {      
 	             result = {   
