059b8b4c4d3ef794c8e333dc2ee3d914575c267d
lafitewu <863512936@qq.com>
2018-07-24 20:07:44 +0800

    倒序渲染

--- db/usersql.js
+++ db/usersql.js
@@ -1,8 +1,8 @@
 var UserSQL = {  
 	insert:'INSERT INTO htt_tab_dynamic_list(id,title,resource,url,type,images,status,createDate) VALUES(?,?,?,?,?,?,?,NOW())', 
 	querypages: 'select id from htt_tab_dynamic_list',
-	queryAll:'SELECT id,type,title,info,resource,url,html,status,images FROM htt_tab_dynamic_list limit ?,?', 
-	queryAdmin: 'select * from htt_tab_dynamic_list limit ?,?',
+	queryAll:'SELECT id,type,title,info,resource,url,html,status,images FROM htt_tab_dynamic_list order by id desc limit ?,?', 
+	queryAdmin: 'select * from htt_tab_dynamic_list order by id desc limit ?,?',
 	queryImg:'SELECT images FROM htt_tab_dynamic_list', 
 	getUserById:'SELECT * FROM user WHERE id = ? ',
 	getUserByName:'SELECT * FROM user WHERE name = ? ',
