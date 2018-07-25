db9823dcbc43cda897fb5cc67da08279c13e71f6
lafitewu <863512936@qq.com>
2018-07-25 20:03:22 +0800

    解决id溢出bug

--- db/config.js
+++ db/config.js
@@ -1,13 +1,13 @@
 module.exports = {  
 	mysql: {   
-		// host: 'rm-wz9u8a3hka2dmzt28.mysql.rds.aliyuncs.com',
-		host: '182.92.82.188',     
-		// user: 'midong',   
-		user: 'root',
-		// password: 'Midong2018', 
-		password: 'Yfax2016',  
-		// database:'cms',
-		database:'cfdb',
-		port: 33061
+		host: 'rm-wz9u8a3hka2dmzt28.mysql.rds.aliyuncs.com',
+		// host: '182.92.82.188',     
+		user: 'midong',   
+		// user: 'root',
+		password: 'Midong2018', 
+		// password: 'Yfax2016',  
+		database:'cms',
+		// database:'cfdb',
+		port: 3306
 	}
 };
