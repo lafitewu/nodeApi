0c1f88afd7c1d840b35486f01175c13aa33d7d0a
lafitewu <863512936@qq.com>
2018-07-25 16:59:40 +0800

    连接正式服数据库

--- db/config.js
+++ db/config.js
@@ -1,9 +1,9 @@
 module.exports = {  
 	mysql: {   
-		host: '182.92.82.188',     
-		user: 'root',   
-		password: 'Yfax2016',  
-		database:'cfdb',
-		port: 33061
+		host: 'rm-m5e100gk3x226m0j4rw.mysql.rds.aliyuncs.com',     
+		user: 'lttuser',   
+		password: 'lttGo888',  
+		database:'htt',
+		port: 3306
 	}
 };
