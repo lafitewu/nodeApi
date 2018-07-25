db054b690dea5a239710a395821fe3f592086382
lafitewu <863512936@qq.com>
2018-07-25 19:09:14 +0800

    回归测试

--- db/config.js
+++ db/config.js
@@ -1,9 +1,13 @@
 module.exports = {  
 	mysql: {   
-		host: 'rm-wz9u8a3hka2dmzt28.mysql.rds.aliyuncs.com',     
-		user: 'midong',   
-		password: 'Midong2018',  
-		database:'cms',
-		port: 3306
+		// host: 'rm-wz9u8a3hka2dmzt28.mysql.rds.aliyuncs.com',
+		host: '182.92.82.188',     
+		// user: 'midong',   
+		user: 'root',
+		// password: 'Midong2018', 
+		password: 'Yfax2016',  
+		// database:'cms',
+		database:'cfdb',
+		port: 33061
 	}
 };
