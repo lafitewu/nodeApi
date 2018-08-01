154b28f82d28efa3e8bfec2a641f2704e86e4f4a
lafitewu <863512936@qq.com>
2018-08-01 16:06:28 +0800

    修复后台和api权重不一致

--- db/usersql.js
+++ db/usersql.js
@@ -2,7 +2,7 @@ var UserSQL = {
 	insert:'INSERT INTO htt_tab_dynamic_list(id,category,seqNum,title,resource,url,type,images,status,createDate) VALUES(?,?,?,?,?,?,?,?,?,NOW())', 
 	querypages: 'select id from htt_tab_dynamic_list',
 	queryAll:'SELECT id,type,title,info,resource,url,html,status,images FROM htt_tab_dynamic_list where category = ? and status = 2 order by seqNum desc,createDate desc limit ?,?', 
-	queryAdmin: 'select * from htt_tab_dynamic_list order by id desc limit ?,?',
+	queryAdmin: 'select * from htt_tab_dynamic_list order by seqNum desc,createDate desc limit ?,?',
 	queryImg:'SELECT images FROM htt_tab_dynamic_list', 
 	getUserById:'SELECT * FROM user WHERE id = ? ',
 	getUserByName:'SELECT * FROM user WHERE name = ? ',
